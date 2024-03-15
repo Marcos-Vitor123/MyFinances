6adc66ecd784a020a438e0434e7a609d3107f8aa
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-15 20:27:21 -0300

    Criação das variáveis

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@ namespace MyFinances
 {
     internal class Program
     {
-        
+        decimal rendaMensal = 0;
+        decimal rendaExtra = 0;
+        decimal rendaTotal = 0;
+        decimal aluguel = 0;
+        decimal internet = 0;
+        decimal luz = 0;
+        decimal cursoProgramacao = 0;
+        decimal faculdade = 0;
+        decimal nubank = 0;
+        decimal neon = 0;
+        decimal inter = 0;
+        decimal telefone = 0;
+        decimal estetica = 0;
+        decimal entretenimento = 0;
     }
 }
 
