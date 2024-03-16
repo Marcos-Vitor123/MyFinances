e2945d5797cac9a4b5d061b47c03d1c6cbe0d2de
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-15 21:08:30 -0300

    Impreensões na tela

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace MyFinances
         decimal rendaMensal = 0;
         decimal rendaExtra = 0;
         decimal rendaTotal = 0;
+        decimal valorRestante = 0;
         decimal aluguel = 0;
         decimal internet = 0;
         decimal luz = 0;
@@ -21,6 +22,34 @@ namespace MyFinances
         decimal telefone = 0;
         decimal estetica = 0;
         decimal entretenimento = 0;
+
+        // Impressões na tela
+
+        Console.Clear();
+        Console.WriteLine("\n\tMinhas Finanças\t\n");
+
+        Console.WriteLine("\tRenda Mensal\t\n");
+
+        Console.WriteLine($"Renda Mensal: \t\t{rendaMensal}");
+        Console.WriteLine($"Renda Extra: \t\t{rendaExtra}");
+        Console.WriteLine($"Renda Total: \t\t{rendaTotal}\n");
+
+        Console.WriteLine("\tGastos Mensal\t\n");
+
+        Console.WriteLine($"Aluguel: \t\t{aluguel}");
+        Console.WriteLine($"Internet: \t\t{internet}");
+        Console.WriteLine($"Luz: \t\t\t{luz}");
+        Console.WriteLine($"Rocketseat: \t\t{cursoProgramacao}");
+        Console.WriteLine($"Ampli: \t\t\t{faculdade}");
+        Console.WriteLine($"Nubank: \t\t{nubank}");
+        Console.WriteLine($"Neon: \t\t\t{neon}");
+        Console.WriteLine($"Inter: \t\t\t{inter}");
+        Console.WriteLine($"Celular: \t\t{telefone}");
+        Console.WriteLine($"Corte Cabelo: \t\t{estetica}");
+        Console.WriteLine($"Amazon: \t\t{entretenimento}\n");
+
+        Console.WriteLine($"Valor Restante: \t{valorRestante}\n");
+        Console.ReadKey();
         }
     }
 }
